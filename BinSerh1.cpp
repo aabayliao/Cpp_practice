e762cb1f2a88566557def901f932afde4a8eca66
= <=>
2023-10-18 14:26:09 +0800

    Practice to learn std:cin.
    1. Learn the std:cin and >> symbol.
    2. Know the key of "End of File", ctrl+d in Linux, ctrl+a in windows.
    
    Changes to be committed:
    	new file:   BinSerh1.cpp
    	new file:   BinSerh1.cpp~
    	new file:   BinSerh1.exe
    	new file:   BinSerh1.o
    	deleted:    prog3.cpp~
    	new file:   prog4.cpp
    	new file:   prog4.cpp~
    	new file:   prog4.exe
    	new file:   prog4.o

---
+++ BinSerh1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	vector<int>v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(6);
+	v.push_back(8);
+	vector<int>::iterator it;
+	for(it=v.begin();it!=v.end();it++){
+		printf("%d\n", *it);
+	} 
+	//if(binary_search(v.begin(),v.end(),3))
+	//	cout << "Element is present" << endl;
+	//else
+	//	cout << "Element is not preset" << endl;
+	return 0;
+}
